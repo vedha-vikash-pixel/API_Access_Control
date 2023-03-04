1c3ffad5ea26a6a6732eb483ad30d3f22f065bfa
Vedha Vikash <vikashvedha@gmail.com>
2023-03-04 11:48:19 +0530

    Base project with database configured

---
+++ src/main/java/com/vikash/API_Access_Control/ApiAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.vikash.API_Access_Control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAccessControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikash/API_Access_Control/ApiAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikash.API_Access_Control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
