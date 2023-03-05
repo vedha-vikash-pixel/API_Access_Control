d7dc87116355faf4581b93bda86f00e3dad11e1a
Vedha Vikash <vikashvedha@gmail.com>
2023-03-05 13:41:13 +0530

    created JWT service and method implementations to extract jwt details

--- src/main/java/com/vikash/API_Access_Control/Filter/JwtAuthenticationFilter.java
+++ src/main/java/com/vikash/API_Access_Control/Filter/JwtAuthenticationFilter.java
@@ -2,8 +2,11 @@ package com.vikash.API_Access_Control.Filter;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.vikash.API_Access_Control.Service.JwtService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -14,17 +17,22 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
+		
+	@Autowired
+	private JwtService JwtService;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException { 
+		
 		
 		final String authHeader = request.getHeader("Authorization");
 		if(authHeader == null || !authHeader.startsWith("Bearer ")) {
 			filterChain.doFilter(request, response);
 			return;
 		}
-		final String jwt = authHeader.substring(7);		
+		final String jwt = authHeader.substring(7);
+		final String userName = JwtService.extractUserName(jwt);
 	}
 
 }
---
+++ src/main/java/com/vikash/API_Access_Control/Service/JwtService.java
@@ -0,0 +1,37 @@
+package com.vikash.API_Access_Control.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+	@Value("${jwt.secret}")
+	private String SECRET_KEY;
+
+	public String extractUserName(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	public Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
+	}
+
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+}
