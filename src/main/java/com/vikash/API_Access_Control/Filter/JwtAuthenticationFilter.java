7948c227464f6f314baf28ab67428c1d6d192dd3
Vedha Vikash <vikashvedha@gmail.com>
2023-03-05 14:33:11 +0530

    Token generation and validation method implementations

--- src/main/java/com/vikash/API_Access_Control/Filter/JwtAuthenticationFilter.java
+++ src/main/java/com/vikash/API_Access_Control/Filter/JwtAuthenticationFilter.java
@@ -3,6 +3,9 @@ package com.vikash.API_Access_Control.Filter;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,7 +23,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 		
 	@Autowired
 	private JwtService JwtService;
-
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException { 
@@ -33,6 +38,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 		}
 		final String jwt = authHeader.substring(7);
 		final String userName = JwtService.extractUserName(jwt);
+		if(userName!=null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
+		}
 	}
 
 }
--- src/main/java/com/vikash/API_Access_Control/Service/JwtService.java
+++ src/main/java/com/vikash/API_Access_Control/Service/JwtService.java
@@ -1,13 +1,18 @@
 package com.vikash.API_Access_Control.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -16,6 +21,17 @@ public class JwtService {
 
 	@Value("${jwt.secret}")
 	private String SECRET_KEY;
+	
+	public String generateToken(UserDetails userDetails) {
+		return generateToken(new HashMap<String,Object>(),userDetails);
+	}
+
+	public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+		return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+	}
 
 	public String extractUserName(String token) {
 		return extractClaim(token, Claims::getSubject);
@@ -33,5 +49,16 @@ public class JwtService {
 	private Key getSignKey() {
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
 		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
+	public boolean isTokenValid(String token,UserDetails userDetails) {
+		String userName = extractUserName(token);
+		if(userName.equals(userDetails.getUsername()) && !isExpired(token))
+			return true;
+		return false;
+	}
+
+	private boolean isExpired(String token) {
+		return extractClaim(token, Claims::getExpiration).before(new Date());
 	}
 }
